d50536f0bfbaa331381e2b60dabe2cf3cac55f58
David Cuerrier <david.cuerrier@gmail.com>
2023-01-17 10:14:02 -0500

    Add service route and mod src

--- simple-webapp-php/src/index.php
+++ simple-webapp-php/src/index.php
@@ -1,8 +1,16 @@
-<?PHP
-		
-	$config = json_decode(file_get_contents("config.json"));
-	printf("Application name: %s \n", $config->appname);
-	printf("Application version: %s \n", $config->version);
-	printf("\nTime: %s", date("H:m:s\n", time()));
+<HTML>
+	<head>
+		<title>Simple webapp PHP</title>
+	</head>
 
-?>
+	<body>
+		<pre>
+		<?PHP		
+			$config = json_decode(file_get_contents("config.json"));
+			printf("Application name: %s \n", $config->appname);
+			printf("Application version: %s \n", $config->version);
+			printf("\nTime: %s", date("H:m:s\n", time()));
+		?>
+		</pre>
+	</body>
+</HTML>
