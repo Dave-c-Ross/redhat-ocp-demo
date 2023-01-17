c37ff7a82ce20e039b73b0163ce2cbe552610e23
David Cuerrier <david.cuerrier@gmail.com>
2023-01-17 11:02:04 -0500

    Up

--- simple-webapp-php/src/index.php
+++ simple-webapp-php/src/index.php
@@ -7,9 +7,9 @@
 		<pre>
 <?PHP		
 	$config = json_decode(file_get_contents("config.json"));
-	printf("Application name: %s", $config->appname);
-	printf("Application version: %s", $config->version);
-	printf("\nTime: %s", date("H:m:s", time()));
+	printf("Application name: %s\n", $config->appname);
+	printf("Application version: %s\n", $config->version);
+	printf("\nTime: %s", date("H:m:s\n", time()));
 ?>
 		</pre>
 	</body>
